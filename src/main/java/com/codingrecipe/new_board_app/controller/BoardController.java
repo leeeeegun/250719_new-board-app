69d249914ea760fc6d771047759bf9151a0fe8e3
leeeeegun <leeeeegun@naver.com>
2025-07-19 12:12:55 +0900

    feat: [이의건] Controller 기본 구성 [NBA-1]

--- src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
+++
@@ -1,4 +0,0 @@
-package com.codingrecipe.new_board_app.controller;
-
-public class BoardController {
-}
---
+++ src/main/java/com/codingrecipe/new_board_app/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.new_board_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
