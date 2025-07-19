7957c8a65c2317bd227c2be6489af31260a96501
leeeeegun <leeeeegun@naver.com>
2025-07-19 12:26:02 +0900

    feat: [이의건] DTO 기본 구성 [NBA-2]

---
+++ src/main/java/com/codingrecipe/new_board_app/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.codingrecipe.new_board_app.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardRepository {
+}
---
+++ src/main/java/com/codingrecipe/new_board_app/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.codingrecipe.new_board_app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+}
