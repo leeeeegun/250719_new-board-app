904abb5dc0f395fd9972e7aae2852d333957b58f
leeeeegun <leeeeegun@naver.com>
2025-07-19 12:35:47 +0900

    feat: [이의건] 게시글 작성화면 기능 추가 [NBA-4]

--- src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
+++ src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
@@ -3,9 +3,15 @@ package com.codingrecipe.new_board_app.controller;
 import com.codingrecipe.new_board_app.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
 }
