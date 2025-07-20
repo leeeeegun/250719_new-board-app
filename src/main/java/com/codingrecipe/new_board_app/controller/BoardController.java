14063f2f0413fbc643eccddc81a2277a830d91da
leeeeegun <leeeeegun@naver.com>
2025-07-20 20:29:54 +0900

    update: [이의건] 게시글 list 출력 메서드 추가 [NBA-8]

--- src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
+++ src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
@@ -4,8 +4,11 @@ import com.codingrecipe.new_board_app.dto.BoardDTO;
 import com.codingrecipe.new_board_app.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +25,13 @@ public class BoardController {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        System.out.println("boardDTOList = " + boardDTOList);
+        return "List";
     }
 }
