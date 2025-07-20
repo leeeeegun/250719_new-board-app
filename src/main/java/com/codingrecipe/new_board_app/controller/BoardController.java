6ecdf774a8261ed958c5749c4c11ffb8becc473f
leeeeegun <leeeeegun@naver.com>
2025-07-20 18:33:45 +0900

    update: 게시글 작성 데이터 컨트롤러에 가져오기 [NBA-5]

--- src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
+++ src/main/java/com/codingrecipe/new_board_app/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.codingrecipe.new_board_app.controller;
 
+import com.codingrecipe.new_board_app.dto.BoardDTO;
 import com.codingrecipe.new_board_app.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,5 +15,11 @@ public class BoardController {
     @GetMapping("/save")
     public String save() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
     }
 }
